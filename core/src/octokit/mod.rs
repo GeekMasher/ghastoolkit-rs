c8e8bac54e499554efb874534a1a42d0dc1a482d
GeekMasher <geekmasher@gmail.com>
2024-03-07 15:14:11 +0000

    feat: update octokit and add docs

--- core/src/octokit/mod.rs
+++ core/src/octokit/mod.rs
@@ -1,3 +1,8 @@
+//! Octokit is a GitHub API client for Rust.
+
+/// GitHub
 pub mod github;
+/// GitHub Models
 pub mod models;
+/// GitHub Repository
 pub mod repository;
--- core/src/octokit/models.rs
+++ core/src/octokit/models.rs
@@ -1,15 +1,28 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
+/// GitHub Message block
 #[derive(Debug, Clone, Serialize, Deserialize, Hash, Eq, PartialEq)]
 pub struct Message {
+    /// The message text
     pub text: String,
 }
 
+/// GItHub Source Location
 #[derive(Debug, Clone, Serialize, Deserialize, Hash, Eq, PartialEq)]
 pub struct Location {
+    /// Path
     pub path: String,
+    /// Start Line
     pub start_line: u32,
+    /// End Line
     pub end_line: u32,
+    /// Start Column
     pub start_column: u32,
+    /// End Column
     pub end_column: u32,
 }
+
+/// GitHub Languages
+pub type GitHubLanguages = HashMap<String, u32>;
