648b14b9451ee32105fe2800ef262276f908778f
GeekMasher <geekmasher@gmail.com>
2024-03-07 15:18:00 +0000

    feat: update errors and main lib

--- core/src/errors.rs
+++ core/src/errors.rs
@@ -1,23 +1,50 @@
 use octocrab::Error as OctocrabError;
 use regex::Error as RegexError;
 
+/// GitHub Advanced Security Toolkit Error
 #[derive(thiserror::Error, Debug)]
 pub enum GHASError {
+    /// Repository Reference Error
     #[error("RepositoryReferenceError: {0}")]
     RepositoryReferenceError(String),
 
+    /// CodeQL Error
+    #[error("CodeQLError: {0}")]
+    CodeQLError(String),
+
+    /// CodeQL Database Error
+    #[error("CodeQLDatabaseError: {0}")]
+    CodeQLDatabaseError(String),
+
+    /// Octocrab Error (octocrab::Error)
     #[error("OctocrabError: {0}")]
     OctocrabError(#[from] OctocrabError),
 
+    /// Regex Error (regex::Error)
     #[error("RegexError: {0}")]
     RegexError(#[from] RegexError),
 
+    /// Io Error (std::io::Error)
+    #[error("IoError: {0}")]
+    IoError(#[from] std::io::Error),
+
+    /// Serde Error (serde_json::Error)
+    #[error("SerdeError: {0}")]
+    SerdeError(#[from] serde_json::Error),
+
+    /// Yaml Error (serde_yaml::Error)
+    #[error("YamlError: {0}")]
+    YamlError(#[from] serde_yaml::Error),
+
+    /// Url Error (url::ParseError)
     #[error("UrlError: {0}")]
     UrlError(#[from] url::ParseError),
 
+    /// Git Errors (git2::Error)
     #[error("GitErrors: {0}")]
     GitErrors(#[from] git2::Error),
 
+    /// Unknown Error
     #[error("UnknownError: {0}")]
     UnknownError(String),
 }
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,9 +1,46 @@
+//! GitHub Advanced Security (GHAS) Toolkit is a library for interacting with various GitHub's API and features.
+//! The main goal of this library is to provide a simple and easy to use interface with these features.
+//!
+//! ## Features
+//!
+//! There are a few features that are currently supported by this library:
+//!
+//! - [x] CodeQL
+//!   - [x] CodeQL CLI
+//!   - [x] CodeQL Database(s)
+//! - [x] GitHub Advanced Security APIs
+//!   - [x] Code Scanning
+//!   - [ ] Secret Scanning
+//!
+//! ## Usage
+//!
+//! ```rust
+//! use ghastoolkit::{GitHub, Repository};
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     // Initialize GitHub using environment variables or github.com
+//!     let github = GitHub::default();
+//!     println!("GitHub :: {}", github);
+//!
+//!     let repository = Repository::parse("geekmasher/ghastoolkit-rs@main")
+//!         .expect("Failed to create Repository");
+//!     println!("Repository :: {}", repository);
+//! }
+//!
 #![forbid(unsafe_code)]
 #![allow(dead_code)]
+#![deny(missing_docs)]
 
+/// CodeQL module is used to interact with CodeQL CLI and databases
+pub mod codeql;
+/// Code Scanning module is used to interact with GitHub's Code Scanning API
 pub mod codescanning;
+/// GHASToolkit errors module contains all the errors that can be thrown by the library
 pub mod errors;
+/// GitHub Octokit client for interacting with GitHub's API endpoints
 pub mod octokit;
+/// GHASToolkit utils module contains all the utility functions and helpers
 pub mod utils;
 
 /// GitHub API errors
@@ -11,3 +48,8 @@ pub use errors::GHASError;
 /// GitHub API client
 pub use octokit::github::GitHub;
 pub use octokit::repository::Repository;
+
+// CodeQL
+pub use codeql::CodeQL;
+pub use codeql::CodeQLDatabase;
+pub use codeql::CodeQLDatabases;
