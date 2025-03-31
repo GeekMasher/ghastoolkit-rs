9549ecd68a4991d95d3321f367394502063ddf7c
GeekMasher <geekmasher@gmail.com>
2025-03-31 12:39:32 +0100

    feat(codeql): Update CodeQL CLI

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -31,6 +31,7 @@
 #![forbid(unsafe_code)]
 #![allow(dead_code)]
 #![deny(missing_docs)]
+#![doc = include_str!("../../README.md")]
 
 pub mod codeql;
 pub mod codescanning;
@@ -46,10 +47,9 @@ pub use octokit::github::GitHub;
 pub use octokit::repository::Repository;
 
 // CodeQL
+pub use codeql::extractors::{BuildMode, CodeQLExtractor};
 pub use codeql::packs::{CodeQLPack, CodeQLPackType, CodeQLPacks};
-pub use codeql::CodeQL;
-pub use codeql::CodeQLDatabase;
-pub use codeql::CodeQLDatabases;
+pub use codeql::{CodeQL, CodeQLDatabase, CodeQLDatabases};
 
 // Supply Chain
 pub use supplychain::{Dependencies, Dependency};
