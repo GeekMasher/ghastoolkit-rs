9b132094f833d372d784464086e466e07b13c80b
GeekMasher <geekmasher@gmail.com>
2025-05-13 11:16:08 +0100

    feat(codeql): Update CodeQL::run function to better support args

--- core/src/codeql/cli.rs
+++ core/src/codeql/cli.rs
@@ -10,11 +10,12 @@ use crate::{
     codeql::{CodeQLLanguage, database::handler::CodeQLDatabaseHandler},
 };
 
+pub mod builder;
 mod models;
 
-use models::ResolvedLanguages;
-
 use super::{CodeQLExtractor, languages::CodeQLLanguages};
+pub use builder::CodeQLBuilder;
+use models::ResolvedLanguages;
 
 /// CodeQL CLI Wrapper to make it easier to run CodeQL commands
 #[derive(Debug, Clone)]
@@ -107,6 +108,11 @@ impl CodeQL {
         }
     }
 
+    /// Add an external extractor to the CodeQL CLI (search path)
+    pub fn add_extractor(&mut self, extractor: &CodeQLExtractor) {
+        self.search_path.push(extractor.path.clone());
+    }
+
     /// Find CodeQL CLI on the system (asynchronous)
     pub async fn find_codeql() -> Option<PathBuf> {
         // Root CodeQL Paths
@@ -179,7 +185,8 @@ impl CodeQL {
     }
 
     /// Run a CodeQL command asynchronously
-    pub async fn run(&self, args: Vec<&str>) -> Result<String, GHASError> {
+    pub async fn run(&self, args: Vec<impl AsRef<str>>) -> Result<String, GHASError> {
+        let args: Vec<String> = args.iter().map(|arg| arg.as_ref().to_string()).collect();
         debug!("CodeQL::run({:?})", args);
 
         let mut cmd = tokio::process::Command::new(&self.path)
@@ -219,7 +226,7 @@ impl CodeQL {
     /// Run a CodeQL command without showing the output
     ///
     /// This is an internal function and should not be used directly.
-    pub(crate) async fn rn(&self, args: Vec<&str>) -> Result<String, GHASError> {
+    pub(crate) async fn rn(&self, args: Vec<impl AsRef<str>>) -> Result<String, GHASError> {
         let mut codeql = self.clone();
         codeql.showoutput = false;
         codeql.run(args).await
@@ -348,109 +355,5 @@ impl Default for CodeQL {
             additional_packs: Vec::new(),
             showoutput: true,
         }
-    }
-}
-
-/// CodeQL Builder to make it easier to create a new CodeQL instance
-#[derive(Debug, Clone, Default)]
-pub struct CodeQLBuilder {
-    path: Option<String>,
-
-    threads: usize,
-    ram: usize,
-
-    search_paths: Vec<PathBuf>,
-    additional_packs: Vec<String>,
-    showoutput: bool,
-}
-
-impl CodeQLBuilder {
-    /// Set the path to the CodeQL CLI
-    ///
-    /// ```rust
-    /// use ghastoolkit::codeql::cli::CodeQL;
-    ///
-    /// # #[tokio::main]
-    /// # async fn main() {
-    /// let codeql = CodeQL::init()
-    ///     .path("/path/to/codeql")
-    ///     .build()
-    ///     .await
-    ///     .expect("Failed to create CodeQL instance");
-    /// # }
-    /// ```
-    pub fn path(mut self, path: impl Into<String>) -> Self {
-        let path = path.into();
-        if !path.is_empty() {
-            self.path = Some(path);
-        }
-        self
-    }
-
-    /// Set manually the threads for CodeQL
-    pub fn threads(mut self, threads: usize) -> Self {
-        self.threads = threads;
-        self
-    }
-
-    /// Set manually the ram for CodeQL
-    pub fn ram(mut self, ram: usize) -> Self {
-        self.ram = ram;
-        self
-    }
-
-    /// Add additional packs to the CodeQL CLI
-    pub fn additional_packs(mut self, path: String) -> Self {
-        self.additional_packs.push(path);
-        self
-    }
-
-    /// Add a search path to the CodeQL CLI
-    ///
-    /// ```rust
-    /// use ghastoolkit::codeql::cli::CodeQL;
-    ///
-    /// # #[tokio::main]
-    /// # async fn main() {
-    /// let codeql = CodeQL::init()
-    ///     .search_path("/path/to/codeql")
-    ///     .build()
-    ///     .await
-    ///     .expect("Failed to create CodeQL instance");
-    /// # }
-    /// ```
-    pub fn search_path(mut self, path: impl Into<PathBuf>) -> Self {
-        self.search_paths.push(path.into());
-        self
-    }
-
-    /// Set the show output flag for the CodeQL CLI
-    pub fn show_output(mut self, show: bool) -> Self {
-        self.showoutput = show;
-        self
-    }
-
-    /// Build the CodeQL instance
-    pub async fn build(&self) -> Result<CodeQL, GHASError> {
-        let path: PathBuf = match self.path {
-            Some(ref p) => PathBuf::from(p),
-            None => match CodeQL::find_codeql().await {
-                Some(p) => p,
-                None => PathBuf::new(),
-            },
-        };
-        log::debug!("CodeQL CLI path: {:?}", path);
-
-        let version: Option<String> = CodeQL::get_version(&path).await.ok();
-
-        Ok(CodeQL {
-            version,
-            path,
-            threads: self.threads,
-            ram: self.ram.into(),
-            additional_packs: self.additional_packs.clone(),
-            search_path: self.search_paths.clone(),
-            showoutput: self.showoutput,
-        })
     }
 }
