52976b08ac1e15067e7008afecb56845fd41ebec
GeekMasher <geekmasher@gmail.com>
2025-06-05 18:02:06 +0100

    feat: Update Core Lib

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -22,4 +22,19 @@ pub use codeql::packs::{CodeQLPack, CodeQLPackType, CodeQLPacks};
 pub use codeql::{CodeQL, CodeQLDatabase, CodeQLDatabases};
 
 // Supply Chain
-pub use supplychain::{Dependencies, Dependency};
+pub use supplychain::{Dependencies, Dependency, License, Licenses};
+
+// Utilities
+pub use utils::sarif::Sarif;
+
+#[doc(hidden)]
+#[allow(unused_imports, missing_docs)]
+pub mod prelude {
+    pub use crate::codeql::extractors::{BuildMode, CodeQLExtractor};
+    pub use crate::codeql::packs::{CodeQLPack, CodeQLPackType, CodeQLPacks};
+    pub use crate::codeql::{CodeQL, CodeQLDatabase, CodeQLDatabases};
+    pub use crate::errors::GHASError;
+    pub use crate::octokit::github::GitHub;
+    pub use crate::octokit::repository::Repository;
+    pub use crate::supplychain::{Dependencies, Dependency, License, Licenses};
+}
